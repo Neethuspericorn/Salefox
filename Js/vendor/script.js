d9949440e4deafea43e79c81739d16d54c483008
spericonSree <sreekanthsb@spericorn.com>
2022-12-09 14:18:22 +0530

    responsive fix

--- Js/vendor/script.js
+++ Js/vendor/script.js
@@ -6,3 +6,9 @@ $(document).ready(function () {
     });
     
 });
+
+// Initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
